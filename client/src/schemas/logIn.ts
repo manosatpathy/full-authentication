bd4438e866a23cac778678921e68c14947071760
manosatpathy <manosatpathy@gmail.com>
2025-08-24 23:38:54 +0530

    feat: normalize login identifier

--- client/src/schemas/logIn.ts
+++ client/src/schemas/logIn.ts
@@ -1,7 +1,11 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  identifier: z.string().nonempty("Email or Username required"),
+  identifier: z
+    .string()
+    .nonempty("Email or Username required")
+    .trim()
+    .toLowerCase(),
   password: z
     .string()
     .min(8, "Password must be at least 8 characters long")
