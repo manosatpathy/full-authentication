22177252f8a955d92c61987dd90ec8988f073dd8
manosatpathy <manosatpathy@gmail.com>
2025-08-29 18:00:34 +0530

    style: add loading spinner

--- client/src/utils/protectedRoutes/AuthenticatedRoute.tsx
+++ client/src/utils/protectedRoutes/AuthenticatedRoute.tsx
@@ -5,7 +5,11 @@ const AuthenticatedRoute = () => {
   const { isAuthenticated, isLoading } = useAppContext();
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex justify-center items-center h-[420px]">
+        <div className="animate-spin rounded-full h-14 w-14 border-t-4 border-b-4 border-blue-400 border-opacity-80"></div>
+      </div>
+    );
   }
   if (!isAuthenticated) {
     return <Navigate to="/auth/login" />;
