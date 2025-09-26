fbbb7fa6836a338fdb696fac4384e6272a4eb984
manosatpathy <manosatpathy@gmail.com>
2025-09-26 23:46:48 +0530

    refactor: clear user cache and redirect to home on success

--- client/src/hooks/useLogout.ts
+++ client/src/hooks/useLogout.ts
@@ -1,19 +1,24 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import * as apiClient from "../api-Client";
+import { type NavigateFunction } from "react-router-dom";
 
 type ToastMessage = {
   message: string;
   type: "SUCCESS" | "ERROR";
 };
 
-export const useLogout = (showToast: (toastMessage: ToastMessage) => void) => {
+export const useLogout = (
+  showToast: (toastMessage: ToastMessage) => void,
+  navigate: NavigateFunction
+) => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: apiClient.logOut,
     onSuccess: async (data) => {
       showToast({ message: data.message, type: "SUCCESS" });
-      await queryClient.invalidateQueries({ queryKey: ["me"] });
+      queryClient.setQueryData(["me"], null);
+      navigate("/");
     },
     onError: (error) => {
       showToast({ message: error.message, type: "ERROR" });
