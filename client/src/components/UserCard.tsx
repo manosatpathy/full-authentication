f3c82fc2e266ac2e4e99ac8e668925c8c14076d0
manosatpathy <manosatpathy@gmail.com>
2025-10-12 22:42:31 +0530

    refactor: simplify UserCard by removing email verification logic

--- client/src/components/UserCard.tsx
+++ client/src/components/UserCard.tsx
@@ -1,30 +1,9 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { RxAvatar } from "react-icons/rx";
 import { useAppContext } from "@/context/AppContext";
-import { useMutation } from "@tanstack/react-query";
-import * as apiClient from "../api-Client";
-import { useNavigate } from "react-router-dom";
 
 const UserCard = () => {
-  const { user, showToast } = useAppContext();
-  const navigate = useNavigate();
-
-  const { mutate, isPending } = useMutation({
-    mutationFn: apiClient.resendEmailVerificationMail,
-    onSuccess: (data) => {
-      showToast({ message: "Email verification link send!", type: "SUCCESS" });
-      navigate(`/auth/verify-otp?userId=${user?._id}`, {
-        state: { otpExpires: data.otpExpiry },
-      });
-    },
-    onError: (error) => {
-      showToast({ message: error.message, type: "ERROR" });
-    },
-  });
-
-  const verifyNow = () => {
-    mutate();
-  };
+  const { user } = useAppContext();
 
   return (
     <Card className="w-full max-w-sm max-h-[510px] rounded-2xl shadow-xl border-none bg-gradient-to-br from-white via-gray-50 to-blue-100">
@@ -42,24 +21,9 @@ const UserCard = () => {
         </div>
         <div className="flex flex-col justify-center items-center w-full gap-1">
           <p className="text-gray-600">{user?.email}</p>
-          {user?.email_verified ? (
-            <p className="text-green-600 font-semibold text-xs bg-green-50 px-2 py-1 rounded-full mt-1">
-              Verified
-            </p>
-          ) : (
-            <button
-              onClick={verifyNow}
-              disabled={isPending}
-              className={`font-semibold text-xs bg-red-50 px-2 py-1 rounded-full mt-1 transition 
-    ${
-      isPending
-        ? "text-red-400"
-        : "text-red-600 hover:bg-red-100 cursor-pointer"
-    }`}
-            >
-              Verify now
-            </button>
-          )}
+          <p className="text-green-600 font-semibold text-xs bg-green-50 px-2 py-1 rounded-full mt-1">
+            Verified
+          </p>
         </div>
       </CardContent>
     </Card>
