4db17c04c62fd146c8266a112e10bbd651d75ee5
manosatpathy <manosatpathy@gmail.com>
2025-10-23 09:48:31 +0530

    fix: clear cookies with domain support in production

--- server/src/utils/clearAuthCookies.ts
+++ server/src/utils/clearAuthCookies.ts
@@ -1,19 +1,27 @@
 import { Response } from "express";
 
 export const clearAuthCookies = (res: Response) => {
+  const isProduction = process.env.NODE_ENV === "production";
+  const domain = isProduction ? ".manosatpathy.in" : undefined;
+
   res.clearCookie("accessToken", {
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
+    domain,
   });
+
   res.clearCookie("refreshToken", {
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
+    domain,
   });
+
   res.clearCookie("csrfToken", {
     httpOnly: false,
     secure: true,
     sameSite: "none",
+    domain,
   });
 };
